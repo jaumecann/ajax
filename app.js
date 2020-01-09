e159c3681cec6893e6c42cb0a5740e9b5b3cfa66
jaume <jaumecann@gmail.com>
2020-01-09 10:03:26 +0100

    optimitzacio

--- app.js
+++ app.js
@@ -24,5 +24,33 @@ $('document').ready(function(){
 
 })
 
-
+/// optimització ismael html enlloc append y empty
+/*
+$('document').ready(function(){
+    ​
+        $('#fetch').click( function(){
+              getData();
+    ​
+        }
+      
+        );
+    ​
+    ​
+        function getData(){
+            $.ajax({
+                url: 'http://api.icndb.com/jokes/random',
+                type: 'GET',
+                dataType: 'json',
+                success: function(result){
+                    console.log(result)
+                    $('#joke_area').html(result.value.joke);
+                }
+            })
+        }
+    ​
+    ​
+    })
+    ​
+    
+*/
 
